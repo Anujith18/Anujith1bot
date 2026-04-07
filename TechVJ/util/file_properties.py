c1c61129b24dd856821b111a9bcd7318c6b13743
Anujith18 <anujithk88@gmail.com>
2026-04-07 19:39:31 +0530

    Update file_properties.py

--- TechVJ/util/file_properties.py
+++ TechVJ/util/file_properties.py
@@ -56,7 +56,11 @@ def get_hash(media_msg: Message) -> str:
 
 def get_name(media_msg: Message) -> str:
     media = get_media_from_message(media_msg)
-    return getattr(media, 'file_name', "")
+    file_name = getattr(media, 'file_name', "")
+    
+    if file_name:
+        file_name = file_name.replace('@VJ_Bots', '')
+    return file_name
 
 def get_media_file_size(m):
     media = get_media_from_message(m)
