ce6864e5bf432f66c43548b36b111b6391ff3ed7
Anujith18 <anujithk88@gmail.com>
2026-04-07 13:44:45 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -253,9 +253,9 @@ Nᴀᴍᴇ - {}"""
 
 𝗠𝗲𝘀𝘀𝗮𝗴𝗲 <b>: {}</b>"""
 
-    CAPTION = """<b>📂 ғɪʟᴇɴᴀᴍᴇ : {file_name}
+    CAPTION = """<b>🎬 {file_name}
 
-<b>⚙️ sɪᴢᴇ : {file_size}</b>""" 
+⚙️ Size : {file_size}</b>""" 
 
     IMDB_TEMPLATE_TXT = """
 <b>Query: {qurey}
